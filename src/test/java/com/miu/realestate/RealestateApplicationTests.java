b2d7522987d5fe6774f1d4123a2629b327e4b060
Khadher Alqiari <k.alqiari@floranow.com>
2022-11-20 11:31:27 -0600

    spring template

---
+++ src/test/java/com/miu/realestate/RealestateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.realestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
