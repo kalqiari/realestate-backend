22799688fc2e09a88777abf9cf50cd14abce756a
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 09:20:21 -0600

    Created the class's fields

--- src/main/java/com/miu/realestate/entity/Favorite.java
+++ src/main/java/com/miu/realestate/entity/Favorite.java
@@ -1,4 +1,11 @@
 package com.miu.realestate.entity;
 
+import java.util.Date;
+
 public class Favorite {
+
+    private Long id;
+    private Long userId;
+    private Long propertyId;
+    private Date createAt;
 }
