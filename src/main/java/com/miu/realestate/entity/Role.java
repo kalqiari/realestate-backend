3b29e2bbcf7758cdadf082d4edbeb32052213e01
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 15:18:21 -0600

    Annotations for Lombok and Jpa

--- src/main/java/com/miu/realestate/entity/Role.java
+++ src/main/java/com/miu/realestate/entity/Role.java
@@ -1,7 +1,22 @@
 package com.miu.realestate.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "role")
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
