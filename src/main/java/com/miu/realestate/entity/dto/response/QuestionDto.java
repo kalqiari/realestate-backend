f29cec5e12e346a281ae0e7b17583e60c8694b37
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 11:15:50 -0600

    Initial commit. Created Class fields

---
+++ src/main/java/com/miu/realestate/entity/dto/response/QuestionDto.java
@@ -0,0 +1,11 @@
+package com.miu.realestate.entity.dto.response;
+
+import java.util.Date;
+
+public class QuestionDto {
+
+    private Long userId;
+    private Long propertyId;
+    private String content;
+    private Date createdAt;
+}
