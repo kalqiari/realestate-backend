acdc8574bbe861ee23d0da929a08def70432e1a3
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 14:59:23 -0600

    Added notations for Lombok

--- src/main/java/com/miu/realestate/entity/dto/response/FavoriteDto.java
+++ src/main/java/com/miu/realestate/entity/dto/response/FavoriteDto.java
@@ -1,5 +1,12 @@
 package com.miu.realestate.entity.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FavoriteDto {
 
     private Long userId;
