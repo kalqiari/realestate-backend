695d0f6fc335cacb8740ed521ccd96e827833f92
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 08:23:43 -0600

    Created Mappings for getting all users, get user by id, post a user and delete a user by id

--- src/main/java/com/miu/realestate/controller/admin/UserController.java
+++ src/main/java/com/miu/realestate/controller/admin/UserController.java
@@ -1,4 +1,37 @@
 package com.miu.realestate.controller.admin;
 
+import com.miu.realestate.entity.dto.response.UserDto;
+import com.miu.realestate.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/v1/users")
+    public List<UserDto> getUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/v1/users/{id}")
+    public UserDto getUser(@PathVariable("id") Long id){
+        return userService.findById(id);
+
+    }
+
+    @PostMapping("/v1/users")
+    public void save(@RequestBody UserDto userDto) {
+        userService.save(userDto);
+    }
+
+    @DeleteMapping("/v1/users/{id}")
+    public void deleteById(@PathVariable("id") Long id) {
+        userService.deleteById(id);
+    }
 }
