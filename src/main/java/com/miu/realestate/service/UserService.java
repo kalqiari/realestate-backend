6041689e08a487156404d8edce0250339e879782
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 12:52:29 -0600

    Added an update method.

--- src/main/java/com/miu/realestate/service/UserService.java
+++ src/main/java/com/miu/realestate/service/UserService.java
@@ -1,18 +1,23 @@
 package com.miu.realestate.service;
 
+import com.miu.realestate.entity.User;
 import com.miu.realestate.entity.dto.response.UserDto;
 
 import java.util.List;
 
 public interface UserService {
 
-    List<UserDto> findAll();
+    List<User> findAll();
 
-    UserDto findById(Long id);
+    User findById(Long id);
+
+    UserDto findByIdDto(Long id);
 
     void deleteById(Long id);
 
 //    List<UserDto> findByRole(Role role);
 
     void save(UserDto userDto);
+
+    void update(Long userId, UserDto userDto);
 }
