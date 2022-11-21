d94217dc6cde66b6109e05f169f69a57422c8691
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 11:47:26 -0600

    Initial commit. Created class's fields

--- src/main/java/com/miu/realestate/entity/Photo.java
+++ src/main/java/com/miu/realestate/entity/Photo.java
@@ -1,4 +1,11 @@
 package com.miu.realestate.entity;
 
+import java.util.Date;
+
 public class Photo {
+
+    private Long id;
+    private String url;
+    private Long propertyId;
+    private Date deletedAt;
 }
