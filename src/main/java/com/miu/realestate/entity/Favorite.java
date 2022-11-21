2759edde8e5c04e570d67546e5fe6d802661a6ec
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 14:58:38 -0600

    Added annotations

--- src/main/java/com/miu/realestate/entity/Favorite.java
+++ src/main/java/com/miu/realestate/entity/Favorite.java
@@ -1,9 +1,24 @@
 package com.miu.realestate.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="favorites")
 public class Favorite {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long userId;
     private Long propertyId;
