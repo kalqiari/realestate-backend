83da7c490d98dbeafaffc7d98f9252b71ab2ee9a
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 08:24:35 -0600

    Created UserDto fields

---
+++ src/main/java/com/miu/realestate/entity/dto/response/UserDto.java
@@ -0,0 +1,14 @@
+package com.miu.realestate.entity.dto.response;
+
+public class UserDto {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Long phoneNumber;
+    private String streetAddress;
+    private String address2;
+    private String city;
+    private String state;
+    private Integer zipcode;
+}
