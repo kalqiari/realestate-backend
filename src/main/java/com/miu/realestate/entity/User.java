ee38fb9d675c1dcb9f7647470620de287c6bcd02
Betty <betty@Bettys-MacBook-Air.local>
2022-11-21 15:13:29 -0600

    user updated

--- src/main/java/com/miu/realestate/entity/User.java
+++ src/main/java/com/miu/realestate/entity/User.java
@@ -1,10 +1,21 @@
 package com.miu.realestate.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
+@Data
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String firstName;
     private String lastName;
     private String email;
