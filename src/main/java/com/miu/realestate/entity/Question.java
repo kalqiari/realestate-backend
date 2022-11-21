c7da7a182ba4e2a2c0745c6637f0793469555755
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 15:12:50 -0600

    Added lombok annotations @Table and @Id

--- src/main/java/com/miu/realestate/entity/Question.java
+++ src/main/java/com/miu/realestate/entity/Question.java
@@ -1,9 +1,23 @@
 package com.miu.realestate.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "question")
 public class Question {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long userId;
     private Long propertyId;
