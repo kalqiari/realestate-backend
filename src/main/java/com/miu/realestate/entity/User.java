e098bf94dcf0f2e602e7d33ac391c59f0b269de8
Kevin Muchene <muchenekevin@gmail.com>
2022-11-20 20:52:28 -0600

    Created User fields

--- src/main/java/com/miu/realestate/entity/User.java
+++ src/main/java/com/miu/realestate/entity/User.java
@@ -1,4 +1,24 @@
 package com.miu.realestate.entity;
 
+import java.util.Date;
+
 public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Long phoneNumber;
+    private String streetAddress;
+    private String address2;
+    private String city;
+    private String state;
+    private Integer zipcode;
+
+    private Date deactivate_at;
+    private Date create_at;
+    private Date deleted_at;
+
+    private Integer role_id;
+
 }
