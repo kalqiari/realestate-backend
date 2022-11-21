0eb060b4e9ba171311b7a763c92e5e5391f3efe9
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 11:13:47 -0600

    Initial commit. Created Class fields

---
+++ src/main/java/com/miu/realestate/entity/Question.java
@@ -0,0 +1,13 @@
+package com.miu.realestate.entity;
+
+import java.util.Date;
+
+public class Question {
+
+    private Long id;
+    private Long userId;
+    private Long propertyId;
+    private String content;
+    private Date createAt;
+
+}
