b624837ae70626e31f52b1ed0c7f964212add58e
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 15:06:43 -0600

    Added notations for Lombok

--- src/main/java/com/miu/realestate/entity/dto/response/PhotoDto.java
+++ src/main/java/com/miu/realestate/entity/dto/response/PhotoDto.java
@@ -1,5 +1,13 @@
 package com.miu.realestate.entity.dto.response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PhotoDto {
     private String url;
     private Long propertyId;
