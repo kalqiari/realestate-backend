87f739f4effb961e785b4450244d19cccb6efd07
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 15:09:45 -0600

    Initial commit

--- src/main/java/com/miu/realestate/controller/admin/PhotoController.java
+++ src/main/java/com/miu/realestate/controller/admin/PhotoController.java
@@ -8,28 +8,28 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/v1/photos")
 public class PhotoController {
 
     @Autowired
     PhotoService photoService;
 
-    @GetMapping("/v1/photos")
+    @GetMapping("/")
     public List<PhotoDto> getPhotos() {
         return photoService.findAll();
     }
 
-    @GetMapping("/v1/photos/{id}")
+    @GetMapping("/{id}")
     public PhotoDto getPhoto(@PathVariable("id") Long id) {
         return photoService.findById(id);
     }
 
-    @PostMapping("/v1/photos")
+    @PostMapping("/")
     public void save(@RequestBody PhotoDto photoDto) {
         photoService.save(photoDto);
     }
 
-    @DeleteMapping("/v1/photos/{id}")
+    @DeleteMapping("/{id}")
     public void deleteById(@PathVariable("id") Long id) {
         photoService.deleteById(id);
     }
