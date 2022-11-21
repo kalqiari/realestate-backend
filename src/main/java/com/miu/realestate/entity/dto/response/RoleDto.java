08d41cf89cee77cea0e1894bb9c6149d95c67085
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 15:15:48 -0600

    Added Lombok and Jpa annotations

--- src/main/java/com/miu/realestate/entity/dto/response/RoleDto.java
+++ src/main/java/com/miu/realestate/entity/dto/response/RoleDto.java
@@ -1,5 +1,12 @@
 package com.miu.realestate.entity.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoleDto {
     private String name;
 }
