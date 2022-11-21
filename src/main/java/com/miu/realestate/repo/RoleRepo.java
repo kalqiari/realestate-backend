2ad2abc3bec51ba34eb9b86839124074d5e4701f
Betty <betty@Bettys-MacBook-Air.local>
2022-11-21 15:37:57 -0600

    role updated

--- src/main/java/com/miu/realestate/repo/RoleRepo.java
+++
@@ -1,7 +0,0 @@
-package com.miu.realestate.repo;
-
-import com.miu.realestate.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository<Role, Long> {
-}
