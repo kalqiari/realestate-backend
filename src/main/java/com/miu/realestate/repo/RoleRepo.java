797d5c9e2e3e8a2971a4a5dbf76a0514dd448859
Kevin Muchene <muchenekevin@gmail.com>
2022-11-21 08:56:19 -0600

    Initial commit. Extends JpaRepository

---
+++ src/main/java/com/miu/realestate/repo/RoleRepo.java
@@ -0,0 +1,7 @@
+package com.miu.realestate.repo;
+
+import com.miu.realestate.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepo extends JpaRepository<Role, Long> {
+}
