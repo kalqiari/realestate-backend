92cd771b455768058f92404d57fdc83f7f101d0c
Luwam Tesfamariam Weldemichael <63998708+Luwamcyber@users.noreply.github.com>
2022-11-21 22:31:39 -0600

    User (#10)
    Co-authored-by: Khadher Alqiari <66640422+kalqiari@users.noreply.github.com>

--- src/main/java/com/miu/realestate/entity/dto/response/UserDto.java
+++ src/main/java/com/miu/realestate/entity/dto/response/UserDto.java
@@ -13,10 +13,10 @@ public class UserDto {
     private String firstName;
     private String lastName;
     private String email;
-    private Long phoneNumber;
+    private String phoneNumber;
     private String streetAddress;
     private String address2;
     private String city;
     private String state;
-    private Integer zipcode;
+    private String zipcode;
 }
