6ec08eb4330a4aae509791c5371e094a4039fe90
GraceTatiana <112901608+GraceTatiana@users.noreply.github.com>
2022-11-23 15:53:54 -0600

    fourth commit

--- src/main/java/com/miu/realestate/controller/FavoriteController.java
+++ src/main/java/com/miu/realestate/controller/FavoriteController.java
@@ -2,6 +2,7 @@ package com.miu.realestate.controller;
 import com.miu.realestate.entity.dto.response.FavoriteDto;
 import com.miu.realestate.service.FavoriteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,17 +16,19 @@ public class FavoriteController {
     public FavoriteController( FavoriteService favoriteService){
         this.favoriteService = favoriteService;
     }
-
+    @PreAuthorize("hasRole('CUSTOMER')")
     @GetMapping()
     public List<FavoriteDto> getFavorites() {
         return favoriteService.findAll();
     }
 
+    @PreAuthorize("hasRole('CUSTOMER')")
     @GetMapping("/{id}")
     public FavoriteDto getFavorite(@PathVariable("id") Long id) {
         return favoriteService.findByPropertyId(id);
     }
 
+    @PreAuthorize("hasRole('CUSTOMER')")
     @PostMapping()
     public void save(@RequestBody FavoriteDto favoriteDto) {
         favoriteService.save(favoriteDto);
