8aea1a659e8fea99a75126be1849da4f8a3f565e
Betty <betty@Bettys-MacBook-Air.local>
2022-11-23 14:04:12 -0600

    updated

--- src/main/java/com/miu/realestate/repo/PropertyRepo.java
+++ src/main/java/com/miu/realestate/repo/PropertyRepo.java
@@ -1,15 +1,13 @@
 package com.miu.realestate.repo;
 
 import com.miu.realestate.entity.Property;
-import com.miu.realestate.entity.dto.response.PropertyDto;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PropertyRepo extends JpaRepository<Property, Long> {
+public interface PropertyRepo extends CrudRepository<Property, Long> {
 
     List<Property> findAll();
     List<Property> findAllByNoOfBedRoom(int noOfRoom);
@@ -17,7 +15,4 @@ public interface PropertyRepo extends JpaRepository<Property, Long> {
     List<Property> findAllByAddressStateAndAddressCity(String state);
 
     List<Property> findAllByPropertyType(String type);
-
-
-
 }
